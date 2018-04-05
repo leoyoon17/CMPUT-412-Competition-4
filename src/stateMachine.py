7716638df2456cebfda67f75d24113cf0bd342e1
Jaeyeon Yoon <l.yoon17@gmail.com>
2018-04-05 16:10:06 -0600

    added updated files

--- src/stateMachine.py
+++ src/stateMachine.py
@@ -4,6 +4,7 @@ import time
 from smach import State, StateMachine
 from Localization import Localization
 from Exploration import Exploration
+from Stop import Stop
 
 if __name__ == "__main__":
 	rospy.init_node('state_machine_controller')
@@ -12,6 +13,8 @@ if __name__ == "__main__":
 		StateMachine.add('Localization', Localization(),
 						transitions={'success': 'Exploration'})
 		StateMachine.add('Exploration', Exploration(),
-						transitions={'markerFound': None, 'done': None})
+						transitions={'markerFound': 'Stop', 'done': None})
+		StateMachine.add('Stop', Stop(),
+						transitions={})
 	time.sleep(1)		
 	sm.execute()
